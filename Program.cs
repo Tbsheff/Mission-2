823bfe3ebe3ac5e56cce0820823a38b9d2b518af
Tyler Sheffield <tylersheffield427@gmail.com>
2024-01-16 22:55:18 -0700

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using Mission__2;
+using System;
+using System.Runtime.InteropServices;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Welcome to the dice throwing simulator!\n\nHow many dice rolls would you like to simulate?\n");
+
+        int rolls = int.Parse(Console.ReadLine());
+
+        rollDice simulation = new rollDice();
+
+        Dictionary<int, int> output = simulation.SimulateRolls(rolls);
+
+        Console.WriteLine($"\nDICE ROLLING SIMULATION RESULTS\r\nEach \"*\" represents 1% of the total number of rolls.\r\nTotal number of rolls = {rolls}.\r\n");
+        foreach (var kvp in output)
+        {
+            Console.Write($" {kvp.Key}: ");
+            float percent = (float)kvp.Value / rolls * 100;
+
+            int roundedValue = (int)Math.Round(percent, MidpointRounding.AwayFromZero);
+
+            for (int iCount = 0; iCount < roundedValue; iCount++)
+            {
+                Console.Write("*");
+            }
+            Console.WriteLine();
+        }
+        Console.WriteLine("\nThank you for using the dice throwing simulator. Goodbye!\r\n");
+    }
+}
---
+++ rollDice.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mission__2
+{
+    internal class rollDice
+    {
+        public Dictionary<int, int> SimulateRolls(int numberOfRolls)
+        {
+            Random random = new Random();
+            Dictionary<int, int> results = new Dictionary<int, int>();
+
+            for (int iCount = 2; iCount < 13; iCount++)
+            {
+                results[iCount] = 0;
+            }
+
+            for (int i = 0; i < numberOfRolls; i++)
+            {
+                int dice1 = random.Next(1, 7);
+                int dice2 = random.Next(1, 7);
+
+                int sum = dice1 + dice2;
+
+                if (results.ContainsKey(sum))
+                {
+                    results[sum]++;
+                }
+                else
+                {
+                    results[sum] = 1;
+                }
+            }
+
+            return results;
+        }
+    }
+}
